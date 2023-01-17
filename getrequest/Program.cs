8a0d7b3fc998e5f6984a1271ba977c5ed70c0d09
Darlyn30 <105079724+Darlyn30@users.noreply.github.com>
2023-01-16 23:08:54 -0400

    Add files via upload
    programas pedidos por el profesor

---
+++ getrequest/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("hola ");
+
+// hola mundo
+Console.WriteLine("hello world");
+
+//saludo
+Console.WriteLine("nombre:"); //mostrar por consola
+var nombre = Console.ReadLine(); //input
+Console.WriteLine("hola su nombre es " +  nombre);
+
+
+//cuadrado de la edad
+
+var edad = (18);
+Console.WriteLine("el cuadrado de la edad es:");
+Console.WriteLine(edad*edad);
+
+
