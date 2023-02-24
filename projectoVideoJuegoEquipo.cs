956d469ac07064b9b5615380741b681b91460dd4
Darlyn30 <105079724+Darlyn30@users.noreply.github.com>
2023-02-23 22:05:26 -0400

    Update projectoVideoJuegoEquipo.cs

--- projectoVideoJuegoEquipo.cs
+++ projectoVideoJuegoEquipo.cs
@@ -8,7 +8,7 @@ namespace Primeros_pasos
         static void Main(string[] args)
         {
             
-            Console.WriteLine(@"SALUDOS CAMPEON, BIENVENIDO A LA  DUNGEON: NOMBRE DEL PROYECTO ");
+            Console.WriteLine(@"SALUDOS CAMPEON, BIENVENIDO A LA  DUNGEON: "); // NOMBRE DEL PROYECTO
             Console.WriteLine();
             Console.WriteLine("Presiona cualquier tecla para empezar");
             Console.ReadKey();
@@ -22,50 +22,48 @@ namespace Primeros_pasos
             if ( JUGADOR != 0){
                 //OMAR AQUI PROGRAMAS:
                 if ( JUGADOR == 1)
-{
-    int VIDAF = 4;
-    Console.WriteLine("Felicidaddes, acabas de escoger al mago, tienes {0}/4", VIDAF);
-    Console.WriteLine("Eres un mago con el hechizo de salvar tu vida, y tu objetivo es encontrar el baculo del poder");
-    Console.WriteLine("Cuentas con mana suficiente para salvar tu vida 4 veces, evita morir a toda costa!"); 
+                {
+                    int VIDAF = 4;
+                    Console.WriteLine("Felicidaddes, acabas de escoger al mago, tienes {0}/4", VIDAF);
+                    Console.WriteLine("Eres un mago con el hechizo de salvar tu vida, y tu objetivo es encontrar el baculo del poder");
+                    Console.WriteLine("Cuentas con mana suficiente para salvar tu vida 4 veces, evita morir a toda costa!"); 
 
-    int vidasF = 4; // cantidad de hechizos 
+                    int vidasF = 4; // cantidad de hechizos 
 
-        Console.WriteLine("Tienes dos opciones A o B, dependiendo del camino que escojas tendras que usar un hechizo o no.");
-        Console.WriteLine("Spoiler, no saldras vivo de este dungeon");
-        
-        while (vidasF > 0) // while para mantener las vidas
-        {
-            ConsoleKeyInfo input = Console.ReadKey(true); // recibir la letra del usuario
+                Console.WriteLine("Tienes dos opciones A o B, dependiendo del camino que escojas tendras que usar un hechizo o no.");
+                Console.WriteLine("Spoiler, no saldras vivo de este dungeon");
+                
+                while (vidasF > 0) // while para mantener las vidas
+                {
+                    ConsoleKeyInfo input = Console.ReadKey(true); // recibir la letra del usuario
 
-            Console.WriteLine("Tienes dos opciones A o B, dependiendo del camino que escojas tendras que usar un hechizo o no.");
-            Console.Write(" ");
-            Console.WriteLine("Escuchas un sonido y por el tunel A y nada por el B, por cual tunel iras?");
-            Console.Write(" ");
-            Console.WriteLine("Ingresa tu seleccion A o B");
+                    Console.WriteLine("Tienes dos opciones A o B, dependiendo del camino que escojas tendras que usar un hechizo o no.");
+                    Console.Write(" ");
+                    Console.WriteLine("Escuchas un sonido y por el tunel A y nada por el B, por cual tunel iras?");
+                    Console.Write(" ");
+                    Console.WriteLine("Ingresa tu seleccion A o B");
 
-            if (input.Key == ConsoleKey.A) // si presiona el A 
-            {
-                Console.WriteLine("El tunel A ha sido el correcto, estaras vivo por un rato mas");
-                continue;
-            }
+                    if (input.Key == ConsoleKey.A) // si presiona el A 
+                    {
+                        Console.WriteLine("El tunel A ha sido el correcto, estaras vivo por un rato mas");
+                        continue;
+                    }
 
-            // Si el jugador presiona la B, pierde una vida
-            Console.WriteLine("Al entrar al camino te esperaba un caballero con una bayoneta, utilizaste uno de tus hechizos");
-            vidasF--;
+                    // Si el jugador presiona la B, pierde una vida
+                    Console.WriteLine("Al entrar al camino te esperaba un caballero con una bayoneta, utilizaste uno de tus hechizos");
+                    vidasF--;
 
-            // verificar si posee hechizos disponibles
-            if (vidasF == 0)
-            {
-                Console.WriteLine("El caballero te clavo una flecha con su ballesta y moriste, intentalo de nuevo");
-                break;
-            }
+                    // verificar si posee hechizos disponibles
+                    if (vidasF == 0)
+                    {
+                        Console.WriteLine("El caballero te clavo una flecha con su ballesta y moriste, intentalo de nuevo");
+                        break;
+                    }
 
-            Console.WriteLine("Te quedan: " + " " + vidasF + " hechizos utilizalos sabiamente. ");
-        }
+                    Console.WriteLine("Te quedan: " + " " + vidasF + " hechizos utilizalos sabiamente. ");
+                }
 
-        Console.ReadKey();
-
-
+                Console.ReadKey();
 
                 //OLIVER AQUI PROGRAMAS
                 if ( JUGADOR == 2){
@@ -79,107 +77,50 @@ namespace Primeros_pasos
                 //     Console.WriteLine("Felicidaddes, acabas de escoger al arquero, tiene {0}/350", VIDA);
                 // }
 
-               Console.WriteLine(@"SALUDOS CAMPEON, BIENVENIDO A DUNGEON: ");// nombre del projecto
-
-                Console.WriteLine();
-                Console.WriteLine("Presiona cualquier tecla para empezar");
-                Console.ReadKey();
-                Console.Clear();
-                Console.WriteLine(@"SALUDOS CAMPEON, BIENVENIDO A LA  DUNGEON: ");
-                Console.WriteLine(@"Selecciona uno de los Heroes ");
-                int JUGADOR;
-                //int MAGO, CABALLERO, ARQUERO;
-                Console.WriteLine("MAGO (1), CABALLERO (2), ARQUERO(3), Selecciona 1 2 o 3 segun corresponda: ");
-                JUGADOR = Convert.ToInt32(Console.ReadLine());
-
-
-
                 if(JUGADOR == 3){
-                    int VIDA = 350;
-                    Console.WriteLine($"Muy bien, haz seleccionado el arquero , que cuenta con {VIDA} HP");
+                    int bossHealth = 4;
+                    int VIDA = 5;
+                    Console.WriteLine($"felicidades, elegiste al arquero");
+                    Console.WriteLine($"este cuenta con {VIDA} vidas");
 
-                }
-
-                int bossHealth = 270;
-                Console.WriteLine("te toca pelear contra el boss final");
-                //boss pelea
-
-                //tendras que atacar y recibiras un golpe del boss
-
-
-                Console.WriteLine("atacar S/n"); // JUGAR
-                var ataque = Console.ReadLine();
-                while(ataque != "s"){
-                    Console.WriteLine("debes atacar");
-                    ataque = "";
-                    ataque = Console.ReadLine();
-
-                } 
-
-
-
-                if(ataque == "s"){ // si es true
-                    bossHealth = bossHealth - 30;
-                    Console.WriteLine(bossHealth + "/270");
-                    int VIDA = 350;
-                    VIDA = VIDA - 40;
-                    Console.WriteLine($"su vida actualmente es {VIDA}/350");
-                    Console.WriteLine("la vida actual del boss es: ");
-                
-
-                    while(bossHealth > 0 ){
-                        bossHealth = bossHealth - 50;
-                        Console.WriteLine(bossHealth);
-                        bossHealth = bossHealth-50;
-                        Console.WriteLine(bossHealth);
-                        bossHealth = 0;
-                        Console.WriteLine($"la vida del boss es: {bossHealth}/270" );
-                        
+                    Console.WriteLine("te haz encontrado con el boss final");
+                    Console.WriteLine("deseas atacar S/n");
+                    var atacar = Console.ReadLine();
+                    while(atacar != "s"){ //mientras la respuesta del ataque no sea atacar el juego se termina
+                        Console.WriteLine("no puedes continuar");
+                        Console.WriteLine("para jugar debes atacar");
+                        Console.WriteLine("deseas atacar S/n");
+                        atacar = Console.ReadLine();
                     }
-                while(bossHealth != 0) {
-                    VIDA = VIDA - 30; 
-                    Console.WriteLine(VIDA-30);
-                    VIDA = 0;
-                }
-                if(VIDA == 0){
-                    Console.WriteLine("has perdido");
-                
-                } else {}
-
-                if (bossHealth == 0){
-                    Console.WriteLine("se termino el juego: gracias por jugar");
-                } else {}
-                while(VIDA == 0){
-                    Console.WriteLine("deseas continuar?");
-                    var continuar = Console.ReadLine();
-                    if(continuar == "s"){
-                        
-                Console.WriteLine(@"SALUDOS CAMPEON, BIENVENIDO A DUNGEON: ");// nombre del projecto
-
-                Console.WriteLine();
-                Console.WriteLine("Presiona cualquier tecla para empezar");
-                Console.ReadKey();
-                Console.Clear();
-                Console.WriteLine(@"SALUDOS CAMPEON, BIENVENIDO A LA  DUNGEON: ");
-                Console.WriteLine(@"Selecciona uno de los Heroes ");
-                        JUGADOR = Convert.ToInt32(Console.ReadLine());
+                    if(atacar == "s"){
+                        Console.WriteLine("tienes 3 habilidades disponibles");
+                        Console.WriteLine("habilidades disponibles: 1) flecha explosiva, 2) flecha criptoniana 3) flecha con veneno");
+                        int ataque = Convert.ToInt32(Console.ReadLine());
+                        if(ataque == 1){
+                            Console.WriteLine("usaste la flecha explosiva");
+                            Console.WriteLine($"destruiste al boss que cuenta con {bossHealth-bossHealth} vidas pero no sobreviviste al impacto, y terminaste salvando a la dungeon");
+                            Console.WriteLine("JUEGO TERMINADO");
+                        } else if(ataque == 2){
+                            Console.WriteLine("usaste flecha criptoniana y solo infectaste al boss");
+                            Console.WriteLine($"mataste al boss {bossHealth-bossHealth}");
+                            Console.WriteLine("JUEGO TERMINADO");
+                        } else if(ataque == 3){
+                            Console.WriteLine("usaste flecha con veneno y no le hiciste suficiente daño");
+                            Console.WriteLine($"morirste y contigo toda la dungeon y terminaste con {VIDA - VIDA} vidas");
+                        } else {
+                            // si decide ponerse de trol y elegir una habilidad que no esta entre las opciones, se cierra automaticamente el juego
+                            if(ataque != 1){
+                                Console.WriteLine("JUEGO TERMINADO, NO DECIDISTE JUGAR");
+                            } else if(ataque != 2){
+                                Console.WriteLine("JUEGO TERMINADO, NO DECIDISTE JUGAR");
+                            } else if(ataque != 3){
+                                Console.WriteLine("JUEGO TERMINADO, NO DECIDISTE JUGAR");
+                            }
+                        }
                     }
                 }
-                }
 
 
-
-                // for(var i = bossHealth; i <=0; i-- ){
-                //     Console.WriteLine("la vida actual del boss final es " +bossHealth + "/270");
-                
-                // }
-
-
-
-                // while(JUGADOR == bossHealth){
-                //     bossHealth = bossHealth-30;
-                //     Console.WriteLine(bossHealth);
-                // }
 
 
 
