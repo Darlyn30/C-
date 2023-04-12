37a3f18c6f96f819b42d9cb09beaababc4b43c7d
Darlyn30 <105079724+Darlyn30@users.noreply.github.com>
2023-04-12 14:14:16 -0400

    Add files via upload

---
+++ darlynFiles.py
@@ -0,0 +1,44 @@
+
+#este juego trata sobre una vida
+
+
+
+print("hola mundo")
+print('jugamos?')
+print('Y/N')
+opcion = input('')
+
+
+if opcion == 'y' :
+
+    print('MUY BIEN COMENZEMOS')
+    print('''
+        tengo que ir al super
+        pero solo tengo 200 pesos
+        cobro en 10 dias
+        pero tengo que sobrevivir con esos 200 pesos
+        como debo gastarlo
+        opcion
+        a)
+        b)
+c)...''')
+
+    #SITUACION2
+
+opcion2 = str(input(''))
+if opcion2 == 'a':
+    print('mi loco pero usted se va a morir de hambre')
+    print('YA PERDIO')
+elif opcion2 == 'b':
+    print('NO SE SABE QUE PUEDA PASAR')
+elif opcion2 == 'c':
+    print('mi loco used no podia merendar, ya decuadrate el me^ "pajarazo"')
+
+
+else:
+    print('ADIOS')
+
+
+
+
+
