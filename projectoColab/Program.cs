43c854d5692466c3878b055960771fa37891d8ec
darlyn <zomberkawaiimusic@gmail.com>
2023-04-23 13:59:42 -0400

    projecto colaborativo con los panas

--- projectoColab/Program.cs
+++ projectoColab/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System; 
 namespace colab {
     class Program {
-        static void Main(string[] args){}
+        static void Main(string[] args){
+            // code here!!
+            // aplicacion multifuncion
+        }
     }
 }
